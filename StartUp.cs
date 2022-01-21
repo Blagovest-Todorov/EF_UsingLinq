ea5e59a8031fa855b88596b2b92bc9d243cf710e
Blagovest Todorov <72020361+Blagovest-Todorov@users.noreply.github.com>
2022-01-21 09:03:03 +0200

    Add files via upload

---
+++ StartUp.cs
@@ -0,0 +1,33 @@
+﻿namespace MusicHub
+{
+    using System;
+    using System.Linq;
+    using Data;
+    using Initializer;
+
+    public class StartUp
+    {
+        public static void Main(string[] args)
+        {
+            MusicHubDbContext context = 
+                new MusicHubDbContext();
+
+             context.Albums.FirstOrDefault()
+                .Songs.Add(new Data.Models.Song());
+
+            DbInitializer.ResetDatabase(context);
+
+            //Test your solutions here
+        }
+
+        public static string ExportAlbumsInfo(MusicHubDbContext context, int producerId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public static string ExportSongsAboveDuration(MusicHubDbContext context, int duration)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
